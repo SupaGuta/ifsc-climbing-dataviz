b7683ac6a6014cc8bce2022428364cdddab86b62
Gustave Bernier <gustave.bernier@gmail.com>
2026-01-26 13:52:15 +0100

    Refactor API request headers into a single constant for improved maintainability

--- assets/api_request_tester.py
+++ assets/api_request_tester.py
@@ -10,26 +10,24 @@ import traceback
 
 
 # General config
-BASE_DIR = Path(__file__).resolve().parent
-DB_PATH = BASE_DIR / "database" / "ifsc-data.sqlite"   
+BASE_DIR = Path(__file__).resolve().parent.parent
 API_BASE_URL =  "https://ifsc.results.info/api/v1"
 API_DATA_STRUCT_FOLDER = BASE_DIR / "assets" / "api-data-structures"
+HEADERS = {
+    'X-Csrf-Token': 'as3PvBDSeMFu_fMVW8Qtp6VDdrJrsHsaToMcOCCK-fkVlwUyCXfeDGXOpFrUv1IBnYdUZYgY-uSW-SoIrWN8bQ',
+    'Referer': 'https://ifsc.results.info',
+    'Cookie': 'session_id=_verticallife_resultservice_session=fOdV4TuFGIzU315JSVh5TyUd2PeLcdwC9iy6lpNWDBtpjvUhnQCdDZgR90CO57VRc4OMyowGSrzA%2BczbyKPyCMIMa1yr5%2BojTEzGP2fQei8s6v4tNmyueStVlYL46gBo8HXYC%2Fx0yrvRSAuR2rWU4UPnqa%2FrG66wvDOmBBh86GzbWj2ZBfEnOCnxY1gI1PSKYu%2BW4SZ%2FKPR%2FOyL70oWRWCM3pytRBaRPn%2FKlEksHjM%2B2XlkzNRGQi7lFDDeElvUDTRj5aHR2cXkTl0JOFgRY%2B7LWq5vRH6WKwHCmO2%2BEDZxdtMhFqC7aruunhQ%3D%3D--rHK2FeAgP%2BAcKiNE--TvOXDr4r0Rxitjml2KEIFA%3D%3D',
+}
 _NUMERIC_ID = re.compile(r"^\d+$")
 
 
 # API fetch function
 # (Needed headers found thanks to https://github.com/ChickenNungets/IFSC-data-analysis)
 def fetch_data(path):
-    headers = {
-        'X-Csrf-Token': 'as3PvBDSeMFu_fMVW8Qtp6VDdrJrsHsaToMcOCCK-fkVlwUyCXfeDGXOpFrUv1IBnYdUZYgY-uSW-SoIrWN8bQ',
-        'Referer': 'https://ifsc.results.info',
-        'Cookie': 'session_id=_verticallife_resultservice_session=fOdV4TuFGIzU315JSVh5TyUd2PeLcdwC9iy6lpNWDBtpjvUhnQCdDZgR90CO57VRc4OMyowGSrzA%2BczbyKPyCMIMa1yr5%2BojTEzGP2fQei8s6v4tNmyueStVlYL46gBo8HXYC%2Fx0yrvRSAuR2rWU4UPnqa%2FrG66wvDOmBBh86GzbWj2ZBfEnOCnxY1gI1PSKYu%2BW4SZ%2FKPR%2FOyL70oWRWCM3pytRBaRPn%2FKlEksHjM%2B2XlkzNRGQi7lFDDeElvUDTRj5aHR2cXkTl0JOFgRY%2B7LWq5vRH6WKwHCmO2%2BEDZxdtMhFqC7aruunhQ%3D%3D--rHK2FeAgP%2BAcKiNE--TvOXDr4r0Rxitjml2KEIFA%3D%3D',
-    }
-
     url = API_BASE_URL + path
 
     try:
-        response = requests.get(url, headers=headers, timeout=120)
+        response = requests.get(url, headers=HEADERS, timeout=120)
         if response.status_code == 200:
             
             data_struct_filename = request_to_filename(path)
