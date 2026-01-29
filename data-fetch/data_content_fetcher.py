7ac25ff5f3ef147931fdf857700f30bbdacdcac2
Gustave Bernier <gustave.bernier@gmail.com>
2026-01-29 16:07:10 +0100

    Small refactor beacuse table Season_Leagues is not needed anymore

--- data-fetch/data_content_fetcher.py
+++ data-fetch/data_content_fetcher.py
@@ -32,7 +32,6 @@ db_content_cur = db_content_conn.cursor()
 db_content_cur.executescript('''
 DROP TABLE IF EXISTS Seasons;
 DROP TABLE IF EXISTS Leagues;
-DROP TABLE IF EXISTS Season_Leagues;
                      
 CREATE TABLE IF NOT EXISTS Seasons (
     id INTEGER PRIMARY KEY,
@@ -44,20 +43,12 @@ CREATE TABLE IF NOT EXISTS Leagues (
     id INTEGER PRIMARY KEY,
     name TEXT UNIQUE
 );
-                     
-CREATE TABLE IF NOT EXISTS Season_Leagues (
-    id INTEGER PRIMARY KEY,
-    season_id INTEGER,
-    league_id INTEGER,
-    ifsc_id INTEGER UNIQUE,
-    UNIQUE (season_id, league_id)
-);
 ''')
 
 
 # Gather data from seasons endpoint
 # API : /seasons/id
-# Fill tables: Seasons, Leagues, Season_Leagues
+# Fill tables: Seasons, Leagues
 
 # Retrieve valid seasons' ifsc_ids from struct database
 seasons_ifsc_ids = [int(row[0]) for row in db_struct_cur.execute("SELECT ifsc_id FROM Seasons")]
@@ -69,7 +60,6 @@ data_list, failed_ids = data_fetcher.scrape_parallel("seasons", seasons_ifsc_ids
 # Parse data and save to database
 seasons_count = 0
 leagues_count = 0
-season_leagues_count = 0
 
 try:
     for data in data_list:
@@ -81,28 +71,20 @@ try:
         season_id = db_content_cur.fetchone()[0]
         seasons_count = seasons_count + 1
 
-        # Leagues and Season_Leagues
-        season_leagues = data.get("leagues", None)
-        for season_league in season_leagues:
+        # Leagues
+        leagues = data.get("leagues", None)
+        for league in leagues:
             # Leagues
-            league_name = season_league.get("name", None)
+            league_name = league.get("name", None)
             db_content_cur.execute("INSERT OR IGNORE INTO Leagues (name) VALUES ( ? )", (league_name, )) 
             if db_content_cur.rowcount == 1:  
-                leagues_count = leagues_count + 1   
-
-            row = db_content_cur.execute('SELECT id FROM leagues WHERE name = ? ', (league_name, ))
-            league_id = db_content_cur.fetchone()[0]
-
-            # Season_Leagues
-            season_league_ifsc_id = int(season_league.get("url", None).strip('/').split('/')[3])
-            db_content_cur.execute("INSERT OR IGNORE INTO Season_Leagues (season_id, league_id, ifsc_id) VALUES ( ?, ?, ? )", (season_id, league_id, season_league_ifsc_id))
-            season_leagues_count = season_leagues_count + 1
+                leagues_count = leagues_count + 1 
         
         # Commit all season info to database
         db_content_conn.commit()
 
     # Output
-    print(f"Scraped {seasons_count} seasons, {leagues_count} leagues and {season_leagues_count} season leagues.")
+    print(f"Scraped {seasons_count} seasons and {leagues_count} leagues.")
 
 except Exception as e:
     logging.error(f"Error parsing data for '/seasons/{ifsc_id}': {e}")
